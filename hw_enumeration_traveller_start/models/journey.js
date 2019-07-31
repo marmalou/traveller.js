1192f542e8424d4fd5cfa920628081951130119e
marmalou <marmalou2@gmail.com>
2019-07-31 22:53:24 +0100

    first commit

---
+++ hw_enumeration_traveller_start/models/journey.js
@@ -0,0 +1,32 @@
+const Journey = function(startLocation, endLocation, transport, distance) {
+  this.startLocation = startLocation;
+  this.endLocation = endLocation;
+  this.transport = transport;
+  this.distance = distance;
+};
+
+Journey.prototype.startLocation = function () {
+  return this.startLocation.map((location) => {
+    return start.location;
+  });
+};
+
+Journey.prototype.endLocation = function () {
+  return this.endLocation.map((location) => {
+    return end.location;
+  });
+};
+
+Journey.prototype.transport = function (){
+  return this.transport.reduce (transport) => {
+    return total + transport.length;
+});
+
+
+Journey.prototype.distance = function (length) {
+  return this.distance.every((journey) => {
+    return distance.length >= length;
+  });
+
+
+module.exports = Journey;
